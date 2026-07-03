95120928c0228b5854f7b43b6ae85ef860cd9560
hastyar <hastyar.a96@gmail.com>
2026-07-03 14:19:38 +0300

    Initialize Laravel Beacon package with basic structure and configuration files

---
+++ src/Beacon.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Coffessoft\LaravelBeacon;
+
+class Beacon
+{
+    /**
+     * Package version.
+     */
+    public const VERSION = '0.1.0';
+
+    /**
+     * Get the package version.
+     */
+    public function version(): string
+    {
+        return self::VERSION;
+    }
+}
